a726618c48078cb2c4b6362a0b16d1d96359e5ca
Macover <macover1507@hotmail.com>
2022-05-14 18:26:41 -0500

    display agregado con evento onClick

--- app/src/main/java/com/example/calculadora/MainActivity.java
+++ app/src/main/java/com/example/calculadora/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.calculadora;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView display;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        display = (TextView) findViewById(R.id.display);
+
+
+    }
+    public void pintaNumero(View vista){
+
+        Button boton = (Button) vista;
+
+        display.setText("numero" + boton.getText());
+
     }
 }
