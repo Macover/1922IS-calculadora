46fb8b4994727b7b1f7ba2a5d464301ee398b075
Fabian0121 <fabianjuarezsandoval@gmail.com>
2022-05-16 12:11:51 -0500

    Metodo de operaciones v2.0

--- app/src/main/java/com/example/calculadora/MainActivity.java
+++ app/src/main/java/com/example/calculadora/MainActivity.java
@@ -1,47 +1,121 @@
 package com.example.calculadora;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import org.mariuszgromada.math.mxparser.*;
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import java.util.ArrayList;
+import java.util.List;
 /*import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;*/
 
 
-
 public class MainActivity extends AppCompatActivity {
     private EditText display;
     private String cadenaDisplay = "";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         display = (EditText) findViewById(R.id.display);
     }
-    public void pintaTexto(View vista){
 
+    public void pintaTexto(View vista) {
         Button boton = (Button) vista;
         cadenaDisplay += boton.getText();
         display.setText(cadenaDisplay);
-
     }
-    public void limpiarDisplay(View vista){
+
+    public void pintaTexto2(View vista) {
+        Button boton = (Button) vista;
+        cadenaDisplay += " " + boton.getText() + " ";
+        display.setText(cadenaDisplay);
+    }
+
+    public void limpiarDisplay(View vista) {
         cadenaDisplay = "";
         display.setText(cadenaDisplay);
     }
-    public void calcularOperacion(View vista){
-        Expression exp= new Expression(this.cadenaDisplay);
+
+    public void calcularOperacion(View vista) {
+        Expression exp = new Expression(this.cadenaDisplay);
         String result = String.valueOf(exp.calculate());
 
-        if(result != "NaN") {
+        if (result != "NaN") {
             this.display.setText(result);
         } else {
             this.display.setText("Syntax Error!");
         }
         this.cadenaDisplay = "";
+    }
+
+    //Calcular operacion Metodo2
+    public void calcularoperacion2(View view) {
+        String[] display = cadenaDisplay.split(" ");
+        List<String> listAux = new ArrayList<>();
+        for (int a = 0; a < display.length; a++) {
+            listAux.add(display[a]);
+        }
+        double res = 0;
+        String[] opds = {"/", "*", "+", "-"};
+        for (int j = 0; j < opds.length; j++) {
+            int i = 0;
+            for (i = 0; i < listAux.size(); i++) {
+                if (opds[j].equals("/") && listAux.get(i).equals("/")) {
+                    res = Double.parseDouble(listAux.get(i - 1)) / Double.parseDouble(listAux.get(i + 1));
+                    //Correciones
+                    int b = i + 1;
+                    listAux.remove(b);
+                    b = i - 1;
+                    listAux.set(b, String.valueOf(res));
+                    listAux.remove(i);
+                    i = 0;
+                }
+                if (opds[j].equals("*") && listAux.get(i).equals("*")) {
+                    res = Double.parseDouble(listAux.get(i - 1)) * Double.parseDouble(listAux.get(i + 1));
+                    //Correciones
+                    int b = i + 1;
+                    listAux.remove(b);
+                    b = i - 1;
+                    listAux.set(b, String.valueOf(res));
+                    listAux.remove(i);
+                    i = 0;
+                }
+                if (opds[j].equals("+") && listAux.get(i).equals("+")) {
+                    res = Double.parseDouble(listAux.get(i - 1)) + Double.parseDouble(listAux.get(i + 1));
+                    //Correciones
+                    int b = i + 1;
+                    listAux.remove(b);
+                    b = i - 1;
+                    listAux.set(b, String.valueOf(res));
+                    listAux.remove(i);
+                    i = 0;
+                }
+                if (opds[j].equals("-") && listAux.get(i).equals("-")) {
+                    res = Double.parseDouble(listAux.get(i - 1)) - Double.parseDouble(listAux.get(i + 1));
+                    //Correciones
+                    int b = i + 1;
+                    listAux.remove(b);
+                    b = i - 1;
+                    listAux.set(b, String.valueOf(res));
+                    listAux.remove(i);
+                    i = 0;
+                }
+            }
+        }
+        if(listAux.get(0) != null && listAux.size() == 1)
+            this.display.setText(listAux.get(0));
+        else
+            this.display.setText("!Syntaxis Error");
+
+        this.cadenaDisplay = "";
+
     }
 }
